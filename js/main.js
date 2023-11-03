325e093e3af005357f37f8539f6e7cacc8e153bf
BellaZhukova <148293805+BellaZhukova@users.noreply.github.com>
2023-11-04 04:52:37 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,95 +7,179 @@ function AddFile(){
             let content = e.target.result;
             let parse = JSON.parse(content);
             console.log(parse);
+            form(parse)
+
+                }
+            }
+        )
+    }
+
+function form (parse){
+    let form = document.createElement('form') //создание формы
+    form.setAttribute('action', '#')
+    form.setAttribute('class','form')
+    document.body.appendChild(form)
+
+    function name(){
+        let name = document.createElement('p')
+        name.setAttribute('class', 'name')
+        name.innerHTML = parse.name
+        form.appendChild(name)
+    }
+
+    function fields(){
+
+        let div = document.createElement('div')
+        div.setAttribute('class', 'form__inputs')
+        form.appendChild(div)
+
+        for (let field in parse.fields){ //создание label
+            if(parse.fields[field].label){
+                let label = document.createElement('label')
+                label.setAttribute('id', 'label')
+                label.setAttribute('class', 'labelClass')
+                label.innerHTML = parse.fields[field].label
+                div.appendChild(label)
+            }
 
 
-                let form = document.createElement('form') //создание формы
-                form.setAttribute('action', '#')
-                form.setAttribute('class', 'form')
-                document.body.appendChild(form)
+            if(parse.fields[field].input.multiple){
+                let select = document.createElement('select')
+                select.setAttribute('class', 'select')
+                div.appendChild(select)
 
-                let labelInput =  document.createElement('div') //создание контейнера для label и input
-                labelInput.setAttribute('id', 'labelInp')
-                form.appendChild(labelInput)
+                for (let element in parse.fields[field].input.technologies){
+                    let option = document.createElement('option')
+                    option.innerHTML = parse.fields[field].input.technologies[element]
+                    select.appendChild(option)
+                }
+                for (let element in parse.fields[field].input.filetype){
+                    let option = document.createElement('option')
+                    option.innerHTML = parse.fields[field].input.filetype[element]
+                    select.appendChild(option)
+                }
+                if (parse.fields[field].input.technologies){
+                    select.setAttribute('multiple', parse.fields[field].input.technologies)
+                }
+                if (parse.fields[field].input.filetype){
+                    select.setAttribute('filetype', parse.fields[field].input.filetype)
+                }
+            }
+            else if (parse.fields[field].input.type === 'textarea'){
+                let textarea = document.createElement('textarea')
+                textarea.setAttribute('class', 'textarea')
+                textarea.innerHTML = parse.fields[field].input.type
+                div.appendChild(textarea)
 
-                for (let field in parse.fields){ //создание label
-                    let label = document.createElement('label')
-                    label.setAttribute('id', 'label')
-                    label.setAttribute('class', 'labelClass')
-                    // label.setAttribute('for', 'inputs')
-                    label.innerHTML = parse.fields[field].label
-                    labelInput.appendChild(label)
 
-                    let readInput = document.createElement('input') //создание input
-                    readInput.setAttribute('id', 'inputs')
-                    readInput.setAttribute('class', 'input')
-                    readInput.innerHTML = parse.fields[field].input
-                    labelInput.appendChild(readInput)
+                let replace = document.querySelector('textarea')
+                let textValue = replace.value
+                let text = textValue.replace('textarea', '')
+                replace.value = text
 
-                    if (parse.fields[field].label == null){ //вообще
-                        label.setAttribute('hidden', 'true')
-                    } //Почему не работает плак плак
-                    if (parse.fields[field].input.placeholder){
-                        readInput.setAttribute('placeholder', parse.fields[field].input.placeholder)
-                    } //далее у нас идет перебор атрибутов к тегам (выпадающие списки ещё в разработке)
-                    if(parse.fields[field].input.type){
-                        readInput.setAttribute('type', parse.fields[field].input.type)
-                    }
-                    if (parse.fields[field].input.required){
-                        readInput.setAttribute('required', parse.fields[field].input.required)
-                    }
-                    if (parse.fields[field].input.mask){
-                        readInput.setAttribute('mask', parse.fields[field].input.mask)
-                    }
-                    if (parse.fields[field].input.colors){
-                        readInput.setAttribute('colors', parse.fields[field].input.colors)
-                    }
-                    if (parse.fields[field].input.checked){
-                        readInput.setAttribute('checked', parse.fields[field].input.checked)
-                    }
-                    if (parse.fields[field].input.filetype){
-                        readInput.setAttribute('filetype', parse.fields[field].input.filetype)
-                    }
-                    if (parse.fields[field].input.multiple){
-                        readInput.setAttribute('multiple', parse.fields[field].input.multiple)
-                    }
-                    // здесь я как раз пыталась сделать выпадающий список, по итогу получилось не очень(не получилось)
+                if (parse.fields[field].input.type){
+                    textarea.setAttribute('textarea', parse.fields[field].input.type)
+                }
+            }
 
-                    // if (parse.fields[field].input.type === 'technology'){
-                    //     readInput.setAttribute('hidden', 'true')
-                    //     let select = document.createElement('select')
-                    //     readInput.appendChild(select)
-                    // }
+            else{
+                let readInput = document.createElement('input') //создание input
+                readInput.setAttribute('id', 'inputs')
+                readInput.setAttribute('class', 'input')
+                readInput.innerHTML = parse.fields[field].input
+                div.appendChild(readInput)
 
-                    //здесь я пыталась references, но снова же получилось не очень
 
-                    // for(let link in parse.references){
-                    //     let input = document.createElement('input')
-                    //     input.innerHTML = parse.references[link]
-                    //     document.body.appendChild(input)
-                    //
-                    //     if (parse.references[link].type){
-                    //         input.setAttribute('type', parse.references[link].type)
-                    //     }
-                    //     if (parse.references[link].required){
-                    //         readInput.setAttribute('required', parse.references[link].required)
-                    //     }
-                    //     if (parse.references[link].checked){
-                    //         readInput.setAttribute('checked', parse.references[link].checked)
-                    //     }
-                    //
-                    // }
-
-                    //а button ещё не готов
+                if (parse.fields[field].input.placeholder){
+                    readInput.setAttribute('placeholder', parse.fields[field].input.placeholder)
+                } //далее у нас идет перебор атрибутов к тегам (выпадающие списки ещё в разработке)
+                if(parse.fields[field].input.type){
+                    readInput.setAttribute('type', parse.fields[field].input.type)
+                }
+                if (parse.fields[field].input.required){
+                    readInput.setAttribute('required', parse.fields[field].input.required)
+                }
+                if (parse.fields[field].input.mask){
+                    readInput.setAttribute('mask', parse.fields[field].input.mask)
+                    readInput.setAttribute('placeholder', parse.fields[field].input.mask)
+                }
+                if (parse.fields[field].input.colors){
+                    readInput.setAttribute('colors', parse.fields[field].input.colors)
+                }
+                if (parse.fields[field].input.checked){
+                    readInput.setAttribute('checked', parse.fields[field].input.checked)
                 }
             }
         }
-        
-    )
+    }
+    function references(){
+
+        let divRef = document.createElement('div')
+        divRef.setAttribute('id', 'divRef')
+        divRef.setAttribute('class', 'references__div')
+        form.appendChild(divRef)
+
+        for(let link in parse.references){
+            if(parse.references[link].input){
+                let input = document.createElement('input')
+                input.innerHTML = parse.references[link].input
+                divRef.appendChild(input)
+
+                if (parse.references[link].input.type){
+                    input.setAttribute('type', parse.references[link].input.type)
+                }
+            }
+            if(parse.references[link]["text without ref"]){
+                let text_without_ref = document.createElement('p')
+                text_without_ref.textContent = parse.references[link]["text without ref"]
+                divRef.appendChild(text_without_ref)
+            }
+            if(parse.references[link].text){
+                let text = document.createElement('p')
+                text.textContent = parse.references[link].text
+                divRef.appendChild(text)
+            }
+            if(parse.references[link]["ref"]){
+                let ref = document.createElement('a')
+                ref.setAttribute('class', 'link__references')
+                ref.setAttribute('href', '#')
+                ref.textContent = parse.references[link]["ref"]
+                divRef.appendChild(ref)
+            }
+        }
+    }
+
+    function button(){
+        let button__div = document.createElement('div')
+        button__div.setAttribute('class', 'button__div')
+        form.appendChild(button__div)
+        for(let button in parse.buttons){
+            let buttons = document.createElement('button')
+            buttons.innerHTML = parse.buttons[button].text
+            button__div.appendChild(buttons)
+        }
+    }
+    function resButton(){
+        document.getElementById('form').reset()
+    }
+    function res(){
+        document.getElementById('form').reset();
+        let cancel = document.createElement('button');
+        cancel.innerHTML = "Reset";
+        form.document.getElementById('form').appendChild(cancel);
+    }
+    name(parse)
+    fields(parse)
+    references(parse)
+    button(parse)
+    res(parse)
 }
+
+
+
 AddFile() //Неудачная попытка создать форму
-let button = document.createElement('button')
-button.setAttribute('id', 'reset')
-button.reset(AddFile)
-document.body.appendChild(button)
+// let button = document.createElement('button')
+// button.setAttribute('id', 'reset')
+// button.reset(AddFile)
+// document.body.appendChild(button)
 
